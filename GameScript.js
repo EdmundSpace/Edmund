46871b6a6fa18359c695d80b2c2cf54298077110
EdmundSpace <edmundofengland@protonmail.com>
2025-08-23 18:10:30 +0100

    Add files via upload

---
+++ GameScript.js
@@ -0,0 +1,24 @@
+function showTime() {
+	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+}
+showTime();
+setInterval(function () {
+	showTime();
+}, 1000);
+
+function AddTiles() {
+const parentElement = document.getElementByTagName('container');
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 10; j++) {
+      const newElement = document.createElement('img');
+      newElement.src = '/images/HomeFloor.png';
+      parentElement.appendChild(newElement); 
+    }
+  }
+
+  const newElement = document.createElement('div');
+  newElement.setAttribute("id", "messagebar");
+  parentElement.appendChild(newElement); 
+
+} 
