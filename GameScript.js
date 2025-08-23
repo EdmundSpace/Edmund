048cbfb2f04bfefad6e18de55c933067c05cf1f7
EdmundSpace <edmundofengland@protonmail.com>
2025-08-23 19:07:59 +0100

    d

--- GameScript.js
+++ GameScript.js
@@ -7,7 +7,7 @@ setInterval(function () {
 }, 1000);
 
 function AddTiles() {
-const parentElement = document.getElementByTagName('container');
+const parentElement = document.getElementsByTagName('container');
 
   for (let i = 0; i < 9; i++) {
     for (let j = 0; j < 10; j++) {
